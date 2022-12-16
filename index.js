864ab84ec876f3ef803b6a43e302c9e7f0bf4670
dayOne20 <74623326+dayOne20@users.noreply.github.com>
2022-12-16 18:16:32 -0500

    score header updated

--- index.js
+++ index.js
@@ -1,72 +1,62 @@
-
 //--------------------Home-------------------------------
 
-let plusBtnOne = document.getElementsByClassName('btn-one')
-let plusOneScore = document.getElementById('home-score')
-let plusCounter = 0
-   
+let plusBtnOne = document.getElementsByClassName("btn-one");
+let plusOneScore = document.getElementById("home-score");
+let plusCounter = 0;
+
 function plusOne() {
-    plusOneScore.textContent = plusCounter += 1      
+  plusOneScore.textContent = plusCounter += 1;
 }
- 
-    
-let plusBtnTwo = document.getElementById('btn-two')
-let plusTwoScore = document.getElementById('home-score')
+
+let plusBtnTwo = document.getElementById("btn-two");
+let plusTwoScore = document.getElementById("home-score");
 
 function plusTwo() {
-    plusTwoScore.textContent = plusCounter += 2
+  plusTwoScore.textContent = plusCounter += 2;
 }
 
+let plusBtnThree = document.getElementById("btn-three");
+let plusThreeScore = document.getElementById("home-score");
 
-let plusBtnThree = document.getElementById('btn-three')
-let plusThreeScore = document.getElementById('home-score')
-
-function plusThree(){
-    plusThreeScore.textContent = plusCounter += 3
+function plusThree() {
+  plusThreeScore.textContent = plusCounter += 3;
 }
-
 
 // ---------------------Guest-------------------------------
 
-let guestBtnOne = document.getElementById('guest-btn-one')
-let guestScore = document.getElementById('guest-score')
-let guestCounter = 0
+let guestBtnOne = document.getElementById("guest-btn-one");
+let guestScore = document.getElementById("guest-score");
+let guestCounter = 0;
 
-function guestOne(){
-    guestScore.textContent = guestCounter += 1
+function guestOne() {
+  guestScore.textContent = guestCounter += 1;
 }
 
-
-let guestBtnTwo = document.getElementById('guest-btn-two')
-let guestTwoScore = document.getElementById('guest-score')
+let guestBtnTwo = document.getElementById("guest-btn-two");
+let guestTwoScore = document.getElementById("guest-score");
 
 function guestTwo() {
-    guestTwoScore.textContent = guestCounter += 2
+  guestTwoScore.textContent = guestCounter += 2;
 }
 
-
-let guestBtnThree = document.getElementById('guest-btn-three')
-let guestThreeScore = document.getElementById('guest-score')
+let guestBtnThree = document.getElementById("guest-btn-three");
+let guestThreeScore = document.getElementById("guest-score");
 
 function guestThree() {
-    guestThreeScore.textContent = guestCounter += 3
+  guestThreeScore.textContent = guestCounter += 3;
 }
-
 
 //-----------------------Reset------------------------------
 
-let resetScores = document.getElementById('reset-btn')
-let resetHomeScores = document.getElementById('home-score')
-let resetGuestScores = document.getElementById('guest-score')
+let resetScores = document.getElementById("reset-btn");
+let resetHomeScores = document.getElementById("home-score");
+let resetGuestScores = document.getElementById("guest-score");
 
-
-function reset(){
-    guestCounter = 0,
-    plusCounter = 0,
-    resetHomeScores.textContent = 0   
-    resetGuestScores.textContent = 0
-    elem.textContent = timeLeft + ' seconds remaining';
-    countdown();
+function reset() {
+  (guestCounter = 0), (plusCounter = 0), (resetHomeScores.textContent = 0);
+  resetGuestScores.textContent = 0;
+  // elem.textContent = timeLeft + ' seconds remaining';
+  // countdown();
 }
 
 //-----------------------Timer------------------------------
@@ -74,7 +64,7 @@ function reset(){
 // let timeLeft = 5;
 //     let elem = document.getElementById('timer-div');
 //     let timerId = setInterval(countdown, 1000);
-    
+
 //     function cronometer(){
 //         if (plusCounter || guestCounter > 1);
 //         elem.textContent = timeLeft + ' second remaining';
@@ -82,24 +72,22 @@ function reset(){
 //         timeLeft == 0
 //         reset();
 //     }
-        
-    
-    
-    // function countdown() {
-    //   if (timeLeft == -1) {
-    //     clearTimeout(timerId);
-    //     // elem.innerHTML = ' Game Over!';
-    //     reset()
-    //   } else {
-          
-    //       timeLeft
-        //   plusCounter || guestCounter > 1
-        //   countdown
-    //     // elem.innerHTML = timeLeft + ' seconds remaining';
-    //     // timeLeft--;
-    //   }
-    // }
-    
+
+// function countdown() {
+//   if (timeLeft == -1) {
+//     clearTimeout(timerId);
+//     // elem.innerHTML = ' Game Over!';
+//     reset()
+//   } else {
+
+//       timeLeft
+//       plusCounter || guestCounter > 1
+//       countdown
+//     // elem.innerHTML = timeLeft + ' seconds remaining';
+//     // timeLeft--;
+//   }
+// }
+
 //     window.onload = function() {
 // let sec = 5;
 //   setInterval(function() {
@@ -107,13 +95,13 @@ function reset(){
 //     sec--;
 //     if (sec == 00) {
 //     sec = 5
-//     plusCounter && guestCounter 
+//     plusCounter && guestCounter
 
 //   }
 //     }1000);
-    
-//     }
-// setInterval(function(){ alert("Popup window!"); }, 500);
+
+// //     }
+// // setInterval(function(){ alert("Popup window!"); }, 500);
 
 // let timeleft = 3;
 // let downloadTimer = setInterval(function(){
